90da4cb2d620b12fe318e13dbe383bb6f8110b16
Schalk Venter <venterschalk@gmail.com>
2018-10-03 11:06:55 +0200

    Adds example code

--- src/App.js
+++ src/App.js
@@ -1,28 +1,40 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Button from '@material-ui/core/Button';
+import { ContainerQuery } from 'react-container-query';
+
+
+const query = {
+  'large': {
+    minWidth: 600,
+  }
+};
+
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <ContainerQuery query={query}>
+        {query => <Markup query={query} />}
+      </ContainerQuery>
     );
   }
+}
+
+
+const Markup = ({ query }) => {
+  if (query.large) {
+    return (
+      <div style={{ background: 'yellow'}}>
+        <Button variant="contained" color="primary">Large</Button>
+      </div>
+    )
+  }
+
+  return (
+    <div style={{ background: 'orange'}}>
+      <Button variant="contained" color="secondary">small</Button>
+    </div>
+  );
 }
 
 export default App;
