d8d1d5d6de7d4e5f2c07d0786fa6dab22fc0bf78
AliciadW <alicia.dewet@living-stratus.com>
2018-10-08 12:33:59 +0200

    Set up initial styling and layout for select action view

--- src/App.js
+++ src/App.js
@@ -1,40 +1,55 @@
 import React, { Component } from 'react';
-import Button from '@material-ui/core/Button';
-import { ContainerQuery } from 'react-container-query';
+import SelectAction from './components/SelectAction';
 
+import { withStyles } from '@material-ui/core/styles';
 
-const query = {
-  'large': {
-    minWidth: 600,
+import Card from '@material-ui/core/Card';
+import CardHeader from '@material-ui/core/CardHeader';
+
+const styles = {
+  card: {
+    maxWidth: 1140,
+  },
+
+  cardHeader: {
+    backgroundColor: '#004B4F',
+  },
+
+  title: {
+    color:'white',
+  },
+
+  subheader: {
+    color: 'rgba(255, 255, 255, 0.5)'
+  },
+
+  content:{
+    display: 'flex',
+    justifyContent: 'space-between',
+    alignItems: 'center'
   }
 };
 
-
 class App extends Component {
   render() {
+    const { classes } = this.props;
+
     return (
-      <ContainerQuery query={query}>
-        {query => <Markup query={query} />}
-      </ContainerQuery>
+      <Card className={classes.card}>
+        <CardHeader
+          className={classes.cardHeader}
+          classes={{
+            title: classes.title,
+            subheader: classes.subheader,
+            content: classes.content,
+          }}
+          title='Waste removal assistant'
+          subheader='Cape Agulhas Municipality'
+          />
+          <SelectAction />
+      </Card>
     );
   }
 }
 
-
-const Markup = ({ query }) => {
-  if (query.large) {
-    return (
-      <div style={{ background: 'yellow'}}>
-        <Button variant="contained" color="primary">Large</Button>
-      </div>
-    )
-  }
-
-  return (
-    <div style={{ background: 'orange'}}>
-      <Button variant="contained" color="secondary">small</Button>
-    </div>
-  );
-}
-
-export default App;
+export default withStyles(styles)(App);
