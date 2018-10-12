05675977d016b8b8ae19aba75d9565cb85442b86
Schalk Venter <venterschalk@gmail.com>
2018-10-12 20:08:23 +0200

    Removes service worker

--- src/index.js
+++ src/index.js
@@ -11,9 +11,3 @@ const query = {
 };
 
 reactConnection.connect(App, 'waste-removal-widget', query);
-
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
