a132028b4abdc0ea4d57e0e15f2f7856b03bcc0d
Schalk Venter <venterschalk@gmail.com>
2018-10-12 20:18:33 +0200

    Removes service worker reference

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import { render } from 'react-dom';
 import ReactHtmlConnector from 'react-html-connector';
 
 import App from './App';
-import * as serviceWorker from './serviceWorker';
 
 const reactConnection = new ReactHtmlConnector(createElement, render);
 const query = {
