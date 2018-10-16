c4b66185453d16fe9fd63b7537df2b7be6417cfb
AliciadW <alicia.dewet@living-stratus.com>
2018-10-16 11:53:48 +0200

    Cleared ReportContact.jsx linting errors

--- src/components/ReportContact.jsx
+++ src/components/ReportContact.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
 
 import { withStyles } from '@material-ui/core/styles';
@@ -6,8 +6,9 @@ import { withStyles } from '@material-ui/core/styles';
 import { ArrowBack } from '@material-ui/icons';
 import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
+import PropTypes from 'prop-types';
 
-const styles = theme => ({
+const styles = {
 
   container: {
     display: 'flex',
@@ -46,40 +47,46 @@ const styles = theme => ({
     },
   },
 
-});
+};
 
-class ReportContact extends Component {
-  render() {
-    const { classes } = this.props;
+const ReportContact = (props) => {
+  const { classes } = props;
 
-    return (
-      <React.Fragment>
-        <div className={classes.container}>
-          <Button variant="contained" className={classes.button}>
-            <Link to="/" className={classes.link}>
-              <ArrowBack />
-            </Link>
-          </Button>
-          <Typography className={classes.text}>
-            Contact person:
-          </Typography>
-        </div>
-        <Typography className={classes.textSmall}>
-          Mr Walter Linnert
-          <br />
-          <a href="mailto:walterl@capeaghulas.gov.za" className={classes.ref}>
-walterl@capeaghulas.gov.za
-            <br />
-          </a>
-          <a href="tel: +272842556011" className={classes.ref}>
-028 425 56011
-            <br />
-          </a>
-          Dirkie Uys Street, Bredasdorp, 7280
+  return (
+    <React.Fragment>
+      <div className={classes.container}>
+        <Button variant="contained" className={classes.button}>
+          <Link to="/" className={classes.link}>
+            <ArrowBack />
+          </Link>
+        </Button>
+        <Typography className={classes.text}>
+          Contact person:
         </Typography>
-      </React.Fragment>
-    );
-  }
-}
+      </div>
+      <Typography className={classes.textSmall}>
+        Mr Walter Linnert
+        <br />
+        <a href="mailto:walterl@capeaghulas.gov.za" className={classes.ref}>
+          walterl@capeaghulas.gov.za
+          <br />
+        </a>
+        <a href="tel: +272842556011" className={classes.ref}>
+          028 425 56011
+          <br />
+        </a>
+        Dirkie Uys Street, Bredasdorp, 7280
+      </Typography>
+    </React.Fragment>
+  );
+};
+
+ReportContact.defaultProps = {
+  classes: null,
+};
+
+ReportContact.propTypes = {
+  classes: PropTypes.instanceOf(Object),
+};
 
 export default withStyles(styles)(ReportContact);
