864b89369dccf93dcb1a240d85599533b3b02c5c
Schalk Venter <venterschalk@gmail.com>
2018-10-16 12:29:12 +0200

    Move routing logic to DetermineView

---
+++ src/components/DetermineView/index.jsx
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+
+
+class DetermineView extends Component {
+  constructor(...props) {
+    super(...props);
+
+    this.state = {
+      view: 'home',
+      props: {},
+    };
+
+    this.events = {
+      changeView: this.changeView.bind(this),
+    };
+  }
+
+  changeView(view, props) {
+    this.setState({ view, props });
+  }
+
+  render() {
+    const { viewsList } = this.props;
+    const { view, props } = this.state;
+    const { changeView } = this.events;
+    const ViewComponent = viewsList[view];
+    const passedProps = { ...props, changeView };
+    return <ViewComponent {...passedProps} />;
+  }
+}
+
+
+export default DetermineView;
