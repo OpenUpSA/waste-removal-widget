fceb5cd993937ea4265e3ae74fb0366e3b3a5222
Schalk Venter <venterschalk@gmail.com>
2018-10-17 15:52:54 +0200

    Adds core-js polyfills to extend browser support

--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
+import 'core-js/fn/object/assign';
+import 'core-js/fn/promise';
+import 'core-js/es6/map';
+import 'core-js/es6/set';
 import { createElement } from 'react';
 import { render } from 'react-dom';
 import ReactHtmlConnector from 'react-html-connector';
 
 import App from './App';
+
 
 const reactConnection = new ReactHtmlConnector(createElement, render);
 const query = {
