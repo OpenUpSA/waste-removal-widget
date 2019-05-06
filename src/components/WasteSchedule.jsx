a6c8003178b47e2b3d2860ffb4c78740b7207860
Mish <mishuct26@gmail.com>
2019-05-06 14:16:53 +0200

    Destructures Material-ui imported components

--- src/components/WasteSchedule.jsx
+++ src/components/WasteSchedule.jsx
@@ -3,20 +3,21 @@ import PropTypes from 'prop-types';
 
 import { withStyles } from '@material-ui/core/styles';
 
-import Card from '@material-ui/core/Card';
-import CardHeader from '@material-ui/core/CardHeader';
-import CardContent from '@material-ui/core/CardContent';
-import CardActions from '@material-ui/core/CardActions';
+import {
+  Card, 
+  CardHeader,
+  CardContent,
+  CardActions,
+  Button,
+  Typography,
+  InfoOutlinedIcon,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogContentText,
+ } from '@material-ui/core';
 
-import Button from '@material-ui/core/Button/Button';
 import { ArrowBack, Clear } from '@material-ui/icons';
-import Typography from '@material-ui/core/Typography/Typography';
-import InfoOutlinedIcon from '@material-ui/icons/InfoOutlined';
-
-import Dialog from '@material-ui/core/Dialog';
-import DialogActions from '@material-ui/core/DialogActions';
-import DialogContent from '@material-ui/core/DialogContent';
-import DialogContentText from '@material-ui/core/DialogContentText';
 
 const styles = {
   container: {
